870f15f1a611722248ed9eef30fb685330b8114a
Matheus Guedes <mathheus.gr@gmail.com>
2020-10-15 19:22:32 -0300

    Adicionada a entidade Departamento

---
+++ composition1-csharp/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace composition1_csharp.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
