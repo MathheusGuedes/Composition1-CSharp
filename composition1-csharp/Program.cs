e56b15a3f218291bd6cc54486e8aec2b4f7795b7
Matheus Guedes <mathheus.gr@gmail.com>
2020-10-21 11:00:09 -0300

    Instanciando o Department e o Worker associando com o Department

--- composition1-csharp/Program.cs
+++ composition1-csharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using composition1_csharp.Entities;
+using composition1_csharp.Entities.Enums;
+using System;
+using System.Globalization;
 
 namespace composition1_csharp
 {
@@ -6,7 +9,19 @@ namespace composition1_csharp
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter Worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior: ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
             
-        }
     }
 }
